2fcfa619e45a14de11dfbdb34e871b3d395b5458
gheun <gheun098@gmail.com>
2025-02-07 21:52:34 +0900

    ✨[feat] db 테이블 생성

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS msg_apply;
+
+USE msg_apply;
+
+CREATE TABLE IF NOT EXISTS apply_form (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    studentId VARCHAR(20) NOT NULL,
+    tel VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    purpose TEXT NOT NULL,
+    strengths TEXT NOT NULL,
+    failure TEXT NOT NULL,
+    definition TEXT NOT NULL,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+);
